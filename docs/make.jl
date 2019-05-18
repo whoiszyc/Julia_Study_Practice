23ca29cb562f1843a5b06995f092c660bff2d2a7
Yichen <31248391+whoiszyc@users.noreply.github.com>
2019-05-18 12:22:35 -0500

    test docs generation

---
+++ docs/make.jl
@@ -0,0 +1,11 @@
+push!(LOAD_PATH,"/Users/whoiszyc/Github/Julia_Study_Practice/src")
+
+using Documenter
+using Function_Test
+
+makedocs(sitename = "Julia_Study_Practice.jl")
+
+
+# deploydocs(
+#     repo = "github.com/sisl/NeuralVerification.jl.git",
+# )
