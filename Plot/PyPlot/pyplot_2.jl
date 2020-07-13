94b709726cce49ccb5e0a4cf962a714b30a9285f
Yichen <31248391+whoiszyc@users.noreply.github.com>
2020-07-12 23:12:45 -0500

    Update pyplot_2.jl

--- Plot/PyPlot/pyplot_2.jl
+++ Plot/PyPlot/pyplot_2.jl
@@ -1,11 +1,22 @@
+# plot
 using PyPlot
 # If true, return Python-based GUI; otherwise, return Julia backend
 PyPlot.pygui(true)
 rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
-rcParams["font.size"] = 16
-rcParams["font.family"] = "sans-serif"
+rcParams["font.family"] = "Arial"
 
-fig, ax = PyPlot.subplots()
-ax.plot([1, 2, 3], label="test")
-ax.legend()
+xs = 0 : 2π/100 : 2π
+ys1 = sin.(xs)
+ys2 = cos.(xs)
+
+fig, ax = PyPlot.subplots(figsize=(14, 6))
+ax.plot(xs, ys1, "b*-", linewidth=2, label="Sin(x)", markersize=4)
+ax.plot(xs, ys2, "rs-.", linewidth=2, label="Cos(x)", markersize=4)
+ax.legend(loc="upper left'", fontsize=20)
+ax.set_title("Function", fontdict=Dict("fontsize"=>16))
+ax.set_xlabel("Time (min)", fontdict=Dict("fontsize"=>14))
+ax.set_ylabel("Generation capacity", fontdict=Dict("fontsize"=>14))
+ax.xaxis.set_tick_params(labelsize=12)
+ax.yaxis.set_tick_params(labelsize=12)
+fig.tight_layout()
 PyPlot.show()
